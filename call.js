8453355d224d9fc1698b315f8279b536e914f0ee
aishwarya18sr <Aishwarya_S_R@mckinsey.com>
2022-02-16 10:41:14 +0530

    feat: add duck count function using call and filter

---
+++ call.js
@@ -0,0 +1,7 @@
+function duckCount() {
+    return Array.prototype.slice.call(arguments).filter((obj) => {
+      return Object.prototype.hasOwnProperty.call(obj, 'quack')
+    }).length
+  }
+
+  module.exports = duckCount
